519281713aa078cf4a73dde4245ed96c9727b592
mir123456780 <miranda_childs@live.com>
2025-01-19 19:12:56 +0000

    Add files via upload

---
+++ types_practice.py
@@ -0,0 +1,13 @@
+firstname = "Miranda"
+lastname = "Childs"
+
+print(firstname, lastname)
+
+list=[firstname, lastname]
+print(list)
+
+name_dictionary = {'first': firstname, 'last':lastname}
+print(name_dictionary)
+
+print(name_dictionary['first'])
+print(name_dictionary['last'])
